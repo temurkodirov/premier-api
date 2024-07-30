126c5688dfe001cdc8c6c607d8e0d7329c18973a
Temurbek <96979969+temurkodirov@users.noreply.github.com>
2024-07-30 16:57:30 +0500

    no controllers

--- FSSEstate.API/Controllers/AffairController.cs
+++ FSSEstate.API/Controllers/AffairController.cs
@@ -7,89 +7,89 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace FSSEstate.API.Controllers
 {
-    [ApiController]
-    [Route("api")]
-    public class AffairController : BaseController
-    {
-        public AffairController(IService service, IMapper mapper) : base(service, mapper)
-        {
-        }
+    //[ApiController]
+    //[Route("api")]
+    //public class AffairController : BaseController
+    //{
+    //    public AffairController(IService service, IMapper mapper) : base(service, mapper)
+    //    {
+    //    }
 
-        [HttpPost("service/create")]
-        public async Task<IActionResult> Create(AffairCreateRequest affair)
-        {
-            try
-            {
-                bool result = false;
+    //    [HttpPost("service/create")]
+    //    public async Task<IActionResult> Create(AffairCreateRequest affair)
+    //    {
+    //        try
+    //        {
+    //            bool result = false;
 
-                var affairModel = Mapper.Map<AffairCreateModel>(affair);
-                result = await Service.AffairService.CreateAsync(affairModel);
+    //            var affairModel = Mapper.Map<AffairCreateModel>(affair);
+    //            result = await Service.AffairService.CreateAsync(affairModel);
               
-                return Ok(result);
-            }
-            catch (Exception ex)
-            {
-                throw new Exception(ex.Message);
-            }
-        }
+    //            return Ok(result);
+    //        }
+    //        catch (Exception ex)
+    //        {
+    //            throw new Exception(ex.Message);
+    //        }
+    //    }
 
-        [HttpPost("service/delete")]
-        public async Task<IActionResult> Delete(long id)
-        {
-            try
-            {
-                var result = await Service.AffairService.DeleteAsync(id);
-                return Ok(result);
-            }
-            catch (Exception ex)
-            {
-                throw new Exception(ex.Message);
-            }
-        }
+    //    [HttpPost("service/delete")]
+    //    public async Task<IActionResult> Delete(long id)
+    //    {
+    //        try
+    //        {
+    //            var result = await Service.AffairService.DeleteAsync(id);
+    //            return Ok(result);
+    //        }
+    //        catch (Exception ex)
+    //        {
+    //            throw new Exception(ex.Message);
+    //        }
+    //    }
 
-        [HttpGet("service/get-all")]
-        [AllowAnonymous]
-        public async Task<IActionResult> GetAll([FromQuery] AffairFilterParams filterParams)
-        {
-            try
-            {
-                var result = await Service.AffairService.GetAllAsync(filterParams);
-                return Ok(result.ToPagedListData());
-            }
-            catch (Exception ex)
-            {
-                throw new Exception(ex.Message);
-            }
-        }
+    //    [HttpGet("service/get-all")]
+    //    [AllowAnonymous]
+    //    public async Task<IActionResult> GetAll([FromQuery] AffairFilterParams filterParams)
+    //    {
+    //        try
+    //        {
+    //            var result = await Service.AffairService.GetAllAsync(filterParams);
+    //            return Ok(result.ToPagedListData());
+    //        }
+    //        catch (Exception ex)
+    //        {
+    //            throw new Exception(ex.Message);
+    //        }
+    //    }
 
-        [HttpGet("service/get/{id}")]
-        public async Task<IActionResult> Get(long id)
-        {
-            try
-            {
-                var result = await Service.AffairService.GetByIdAsync(id);
-                return Ok(result);
-            }
-            catch (Exception ex)
-            {
-                throw new Exception(ex.Message);
-            }
-        }
+    //    [HttpGet("service/get/{id}")]
+    //    public async Task<IActionResult> Get(long id)
+    //    {
+    //        try
+    //        {
+    //            var result = await Service.AffairService.GetByIdAsync(id);
+    //            return Ok(result);
+    //        }
+    //        catch (Exception ex)
+    //        {
+    //            throw new Exception(ex.Message);
+    //        }
+    //    }
 
-        [HttpPost("service/update/{id}")]
-        public async Task<IActionResult> Update(long id, AffairUpdateRequest affair)
-        {
-            try
-            {
-                var affairUpdateModel = Mapper.Map<AffairUpdateModel>(affair);
-                var result = await Service.AffairService.UpdateAsync(id, affairUpdateModel);
-                return Ok(result);
-            }
-            catch (Exception ex)
-            {
-                throw new Exception(ex.Message);
-            }
-        }
+    //    [HttpPost("service/update/{id}")]
+    //    public async Task<IActionResult> Update(long id, AffairUpdateRequest affair)
+    //    {
+    //        try
+    //        {
+    //            var affairUpdateModel = Mapper.Map<AffairUpdateModel>(affair);
+    //            var result = await Service.AffairService.UpdateAsync(id, affairUpdateModel);
+    //            return Ok(result);
+    //        }
+    //        catch (Exception ex)
+    //        {
+    //            throw new Exception(ex.Message);
+    //        }
+    //    }
 
-    }
+    //}
 }
